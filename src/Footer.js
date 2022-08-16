2d8446c53b580a0d55f17eb7111fb572dcb52087
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-08-15 21:43:14 -0600

    created footer element to import into app.js

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer><h2>&copy Luis Rosales - 2022 </h2></footer>
+    );
+  };
+};
+
+export default Footer;
